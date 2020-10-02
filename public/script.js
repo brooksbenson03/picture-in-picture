c75597c441b4ccecdd0bac2e8f6a1f47b1692b99
Brooks Benson <brooksbenson03@gmail.com>
2020-10-02 17:35:31 -0600

    styles, script: added styles to button, button container. Added picture-in-picture scripting

--- public/script.js
+++ public/script.js
@@ -1,1 +1,22 @@
-console.log("testing")
+const video = document.getElementById('video')
+const button = document.getElementById('button')
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+    video.srcObject = mediaStream
+    video.onloadedmetadata = () => {
+      video.play()
+    }
+  } catch (e) {
+    console.log('select media stream error', e)
+  }
+}
+
+button.addEventListener('click', async function() {
+  button.disabled = true
+  await video.requestPictureInPicture()
+  button.disabled = false
+})
+
+selectMediaStream()
